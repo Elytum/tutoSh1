529fd1792374ca2357fceb66259a326298e5f2aa
Arthur CHAZAL <achazal@e2r3p11.42.fr>
2015-10-02 12:28:32 +0200

    Some work on escaping

---
+++ escaping/main.c
@@ -0,0 +1,165 @@
+#include <stdlib.h>
+#include <string.h>
+#define LINE_SIZE 4096
+#define STRING "Ceci est \" un \test"
+
+#define TEST_SIZE 15
+#include <stdio.h>
+
+#define INTERPRETED 1
+#define TO_INTERPRETE 2
+#define SPACING 3
+#define SIMPLE_QUOTED 4
+#define DOUBLE_QUOTED 5
+#define BACK_QUOTED 6
+#define BACKSLASHED 7
+
+typedef struct	s_env
+{
+	size_t		len;
+	size_t		pos;
+	char		line[LINE_SIZE];
+	char		buffer[LINE_SIZE];
+	char		interprete[LINE_SIZE];
+}				t_env;
+
+void		debug_env(t_env *env)
+{
+	size_t	position;
+
+	printf("Buffer:     {");
+	position = 0;
+	while (position <= env->len)
+	{
+		if (position == 0)
+			printf("'%c'", env->buffer[position]);
+		else if (position != env->len)
+			printf(", '%c'", env->buffer[position]);
+		else
+			printf(", '%c'}.\n", env->buffer[position]);
+		++position;
+	}
+
+	printf("interprete: {");
+	position = 0;
+	while (position <= env->len)
+	{
+		if (position == 0)
+			printf(" %i", env->interprete[position]);
+		else if (position != env->len)
+			printf(",   %i", env->interprete[position]);
+		else
+			printf(",   %i}.\n", env->interprete[position]);
+		++position;
+	}
+	if (env->interprete[env->len] == INTERPRETED || env->interprete[env->len] == SPACING)
+		printf("Line is closed\n");
+	else if (env->interprete[env->len] == SIMPLE_QUOTED)
+		printf("Line is not closed due to a simple quote.\n");
+	else if (env->interprete[env->len] == DOUBLE_QUOTED)
+		printf("Line is not closed due to a double quote.\n");
+	else if (env->interprete[env->len] == BACK_QUOTED)
+		printf("Line is not closed due to a back quote.\n");
+	else if (env->interprete[env->len] == BACKSLASHED)
+		printf("Line is not closed due to a backslash.\n");
+}
+
+void		do_interprete_simple_quote(t_env *env)
+{
+	while (env->pos <= env->len)
+	{
+		if (env->buffer[env->pos] == '\'')
+		{
+			env->interprete[env->pos++] = SIMPLE_QUOTED;
+			return ;
+		}
+		else
+			env->interprete[env->pos++] = SIMPLE_QUOTED;
+	}
+}
+
+void		do_interprete_double_quote(t_env *env)
+{
+	while (env->pos <= env->len)
+	{
+		if (env->buffer[env->pos] == '\"')
+		{
+			env->interprete[env->pos++] = DOUBLE_QUOTED;
+			return ;
+		}
+		else
+			env->interprete[env->pos++] = DOUBLE_QUOTED;
+	}
+}
+
+void		do_interprete_back_quote(t_env *env)
+{
+	while (env->pos <= env->len)
+	{
+		if (env->buffer[env->pos] == '`')
+		{
+			env->interprete[env->pos++] = BACK_QUOTED;
+			return ;
+		}
+		else
+			env->interprete[env->pos++] = BACK_QUOTED;
+	}
+}
+
+void		do_interprete_backslash(t_env *env)
+{
+	env->interprete[env->pos++] = BACKSLASHED;
+}
+
+void		do_interprete_simple(t_env *env)
+{
+	while (env->pos <= env->len)
+	{
+		if (env->buffer[env->pos] == '\'')
+		{
+			env->interprete[env->pos++] = SIMPLE_QUOTED;
+			do_interprete_simple_quote(env);
+		}
+		else if (env->buffer[env->pos] == '\"')
+		{
+			env->interprete[env->pos++] = DOUBLE_QUOTED;
+			do_interprete_double_quote(env);
+		}
+		else if (env->buffer[env->pos] == '\\')
+		{
+			env->interprete[env->pos++] = BACKSLASHED;
+			do_interprete_backslash(env);
+		}
+		else if (env->buffer[env->pos] == '`')
+		{
+			env->interprete[env->pos++] = BACKSLASHED;
+			do_interprete_back_quote(env);
+		}
+		else if (env->buffer[env->pos] == ' ' || env->buffer[env->pos] == '\t')
+			env->interprete[env->pos++] = SPACING;
+		else
+			env->interprete[env->pos++] = INTERPRETED;
+	}
+}
+
+void		do_interprete(t_env *env)
+{
+	env->pos = 0;
+	memcpy(env->buffer, env->line, env->len + 1);
+	memset(env->interprete, '\0', env->len + 1);
+	do_interprete_simple(env);
+	debug_env(env);
+}
+
+int			main(void)
+{
+	t_env	*env;
+
+	if (!(env = (t_env *)malloc(sizeof(t_env))))
+		return (-1);
+	memcpy(env->line, STRING, LINE_SIZE - 1);
+	env->line[LINE_SIZE - 1] = '\0';
+	env->len = strlen(env->line);
+	do_interprete(env);
+	return (0);
+}
