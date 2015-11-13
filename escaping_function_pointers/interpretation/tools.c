f8420fc3827b14c14654f5df7bc9793bcbae55df
Arthur CHAZAL <achazal@e2r4p20.42.fr>
2015-11-13 21:05:13 +0100

    Probably a stupid segfault but I gotta take my bear !!!

--- escaping_function_pointers/interpretation/tools.c
+++ escaping_function_pointers/interpretation/tools.c
@@ -1,6 +1,8 @@
 #include <interprete.h>
 #include <string.h>
 
+#include <unistd.h>
+
 #include <stdio.h>
 
 void		do_interprete(t_env *env)
@@ -47,15 +49,45 @@ int			set_arguments(t_env *env)
 	return (set_argv(env));
 }
 
+void		handle_alias(t_env *env)
+{
+	const char	error[] = "Line too long to handle your alias\n";
+	char	*ptr;
+	char	*alias;
+	char	tmp_char;
+	size_t	len;
+
+	ptr = env->line;
+	while (*ptr && (*ptr != ' ' && *ptr != '\t'))
+		++ptr;
+	tmp_char = *ptr;
+	*ptr = '\0';
+	if ((alias = ht_get(env->alias, env->line)))
+	{
+		len = strlen(alias);
+		if (len + strlen(alias) > sizeof(env->buffer) - 1)
+			write(1, error, sizeof(error) - 1);
+		else
+		{
+			memcpy(env->buffer, alias, len);
+			memcpy(env->buffer + len, env->line, env->len + 1);
+			memset(env->interprete, '\0', env->len + len + 1);
+			*ptr = tmp_char;
+			return ;
+		}
+	}
+	*ptr = tmp_char;
+	memcpy(env->buffer, env->line, env->len + 1);
+	memset(env->interprete, '\0', env->len + 1);
+}
+
 char		start_interprete(t_env *env)
 {
 	env->start = 0;
 	env->last_char = '\0';
 	if (env->len == 0)
 		return (STOP);
-	memcpy(env->buffer, env->line, env->len + 1);
-	memset(env->interprete, '\0', env->len + 1);
-
+	handle_alias(env);
 	do_interprete(env);
 	if (env->last_char)
 		return (NOT_CLOSED);
